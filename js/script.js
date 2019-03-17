e15afc174acecd6c6c07803fd10c4d342ac7b13e
MedicJS <colin.medic@gmail.com>
2019-03-16 18:54:32 -0700

    Added Search Functionality
    You can now filter student list

--- js/script.js
+++ js/script.js
@@ -5,9 +5,19 @@ FSJS project 2 - List Filter and Pagination
    
 //declare global varibles
 const page = document.querySelector('.page');
-const studentList = document.getElementsByClassName('student-item');
-const list = document.querySelector('.student-list').children;
-const numOfPages = Math.ceil(list.length / 10); //Num of Students divided by 10
+//const studentItems = document.getElementsByClassName('student-item');
+const list = document.querySelector('ul.student-list');
+//Gets all the lis of the list above
+const students = list.children;
+//Calcs the amount of pages needed
+//used for remove function and creating search bar
+const pageHeader = document.getElementsByClassName('page-header')[0];
+//used to create pagination div
+const pagDiv = document.createElement('div');
+pagDiv.className = 'pagination';
+//makes pag unordered list of links
+const pagUl = document.createElement('ul');
+
 
 //ShowPage Function 
 const showPage = (list, page) => {
@@ -15,17 +25,15 @@ const showPage = (list, page) => {
    const low = (page * 10) - 10;
    const high = 10 * page;
    //loop through student list using the low and high number
-   for(let i = 0 ; i < studentList.length; i++){
-      const li = studentList[i];
-      const conditional = ((i >= low && i < high)) ? li.style.display = '' : li.style.display = 'none'; //Tenary Operator
+   for(let i = 0 ; i < students.length; i++){
+      const li = students[i];
+      //Example if i >= 1 && 10 display students 1-10 if i >= 11  && 20 display 11-20 and so on
+      ((i >= low && i < high)) ? li.style.display = '' : li.style.display = 'none'; //Tenary Operator
    }
 }
 
-
 //Function to create correct Pagination based off of numOfPages
 const createPagination = (num) => {
-   const pagDiv = document.createElement('div');
-   const pagUl = document.createElement('ul');
    pagDiv.className = 'pagination';
 
    // remove active classes - src https://stackoverflow.com/questions/38990163/how-can-i-add-and-remove-an-active-class-to-an-element-in-pure-javascript/38990288
@@ -35,8 +43,7 @@ const createPagination = (num) => {
       el.classList.remove("active");
       });
       e.target.className = "active";
-   }
-    
+   } 
    //Creates New Pag Link
    const createLi= (i) => {
       const li = document.createElement('li');
@@ -47,28 +54,96 @@ const createPagination = (num) => {
       pagUl.appendChild(li);
       pagDiv.appendChild(pagUl);
    }
-
    //loop over the createLi() to based off num of pages
    for(i = 0; i < num; i++) {
       createLi(i);
    }
-
    //Adds Click event for
    pagUl.addEventListener('click', (e) => {
       removeActive(e);
       const newPage = e.target.innerHTML;
-      showPage(studentList, newPage);
+      showPage(students, newPage);
       e.target.classList = 'active';
    })
    //Appends Created Div to Main Page Element
    page.appendChild(pagDiv);
-
 }// End of Create Pagination Function
 
+const noResDiv = document.createElement('div');
+const noResults = () => {
+   noResDiv.innerHTML = "";
+   noResDiv.innerHTML = "<p>No results were found.</p>";
+   list.appendChild(noResDiv);
+ }
+ 
+
+//Create Search Input Form 
+const searchDiv = document.createElement('div');
+searchDiv.className = 'student-search';
+//Create Input Form
+const searchBar = document.createElement('input');
+searchBar.type = 'text';
+searchBar.placeholder = 'Search For Students...';
+//Create Search Button
+const searchBtn = document.createElement('button');
+searchBtn.textContent = 'search';
+//Append Input and Button to Div and Page
+searchDiv.appendChild(searchBar);
+searchDiv.appendChild(searchBtn);
+pageHeader.appendChild(searchDiv);
+
+//FILTER STUDENT LIST
+
+//create filterStudents Function
+const filterStudents = () => {
+   //Get Input Value That Updates With Keyup
+   let value = searchBar.value.toUpperCase();
+   //Grabs Ul
+   let ul = document.querySelector('ul.student-list');
+   
+   //Grabs each li
+   let li = ul.querySelectorAll('li');
+
+   let resultsArr = [];
+   
+   //iterates through each li above
+   for(let i = 0; i < li.length; i++) {
+         //Grabs The Text inside the h3 of each li in list
+         let names = li[i].getElementsByTagName('h3')[0].innerText;
+
+         //Converts text to Uppercase and use indexOf to test the array.
+         if(names.toUpperCase().indexOf(value) > -1){
+            li[i].style.display = ''; //Shows matching names
+            resultsArr.push
+         } else {
+            li[i].style.display = 'none';//hides not matching names
+         }
+   
+      //Remove Pagination while searching
+      if(value != '') {
+         document.getElementsByClassName('pagination')[0].style.display = 'none';
+      } else {
+      // Add  Pagination class while searching
+         let pages = document.getElementsByClassName('pagination')[0].getElementsByTagName('a');
+         for (let i = 0; i < pages.length; i++) {
+            pages[i].className = "";
+         } //RESET PAGE WITH LINKS
+         showPage(students, 1);
+         pages[0].className = "active";
+         document.getElementsByClassName('pagination')[0].style.display = 'block';
+      }
+   }
+   //calls the no result functions
+   if(resultsArr !== '') {
+      noResults();
+   }
+}
 
 
+//Search Event Listener
+searchBar.addEventListener('keyup', filterStudents);
 //Run Functions on start
-showPage(studentList, 1);
+showPage(students, 1);
 createPagination(numOfPages);
 
 //End
